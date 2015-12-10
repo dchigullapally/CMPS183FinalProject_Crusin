51bb80a4f5787b6188125d05af447ea5a81f9801
dchigullapally <dchigull@ucsc.edu>
2015-12-10 10:52:37 -0800

    Just adding some random files

--- CruzinUCSC/parameters_8000.py
+++ CruzinUCSC/parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$a9fc90add1eb8b3c$853d456ecaf0b5aa6f457eae9a8aeb8e3ac1c937"
+password="pbkdf2(1000,20,sha512)$a93cb6fe3bd8b057$a7a9eec3ff95496be15a6d5bcbc4a0fa62d56555"
